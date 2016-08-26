afbf61fd1ad49619ac7ab2d810fcf559da15dfb5
YuritzaSolis <yuritza.solis01@gmail.com>
2016-08-26 12:35:29 -0600

    formularios de descuento

--- caja.php
+++ caja.php
@@ -81,12 +81,15 @@
                                 $sql="UPDATE caja_tmp SET importe='$aventa', cant='$acant' WHERE cod='$dcodigo'";
                                 $db->mysqli->query($sql);
                             }else{
+                               
                                 $can=$db->mysqli->query("SELECT * FROM producto WHERE cod='$codigo' OR nom='$codigo'");
                                 if($row=$can->fetch_object()){
                                     if($_SESSION['tventa']=="venta"){
+                                        
                                         $importe=$row->venta;
                                         $venta=$row->venta;
                                     }else{
+                                        
                                         $importe=$row->mayor;
                                         $venta=$row->mayor;
                                     }
@@ -95,11 +98,12 @@
                                     $cant="1";
                                     $exitencia=$row->cantidad;
                                     $usu=$_SESSION['username'];
-                                    $sql="INSERT INTO caja_tmp(cod,nom,venta,cant,importe,existencia,usu)VALUES('$cod','$nom','$venta','$cant','$importe','$exitencia','$usu')";
+                                    $sql="INSERT INTO caja_tmp(cod,nom,venta,cant,importe,exitencia,usu)VALUES('$cod','$nom','$venta','$cant','$importe','$exitencia','$usu')";
                                     $db->mysqli->query($sql);
+                                  
                                 }else{
                                     echo '<div class="alert alert-error" align="center">
-                                            <strong>
+                                            <strong>    
                                                 Producto no encontrado en la base de datos 
                                                 <a href="#modal1" role="button" class="modal-trigger waves-effect waves-light btn green accent-4"><i class="material-icons">add</i></a>
                                             </strong>
@@ -116,29 +120,103 @@
                       <br>
                       <div class="chip center-align" style="width:100%;">
                           <img src="img/rosa.jpg" alt="Contact Person">
-                          <b>Nombre del Cajero/a:</b>  Rosa Lumbí
+                          <b>Nombre del Cajero/a:</b> <?php echo $nombre_usu; ?>
                       </div>
                     
                   </div>
                   <div class="col s12">
+                  <?php // $can=$db->mysqli->query("SELECT * FROM caja_tmp WHERE usu='$usuario'"); if($row=$can->fetch_object()){ echo '<div style="overflow:auto;width:100%;top:48px; height:200px">';} ?>
                       <table class="responsive-table">
                           <thead>
                               <tr>
                                   <th>Código</th>
-                                  <th>Descrpcion del Producto</th>
+                                  <th>Descripcion del Producto</th>
                                   <th>Valor Unitario</th>
                                   <th>Cant.</th>
                                   <th>Importe</th>
                                   <th>Existencia</th>
                               </tr>
                           </thead>
-                        
+                          <tbody>
+                          <?php $na=0; $can=$db->mysqli->query("SELECT * FROM caja_tmp WHERE usu='$usuario'");
+                                while($row=$can->fetch_object()){   
+                          ?>
+                                <tr>
+                                    <td><?php echo $row->cod ?></td>
+                                    <td><?php echo $row->nom; ?></td>
+                                    <td><a href="caja.php?id=<?php echo $row->cod.'&ddes='.$_SESSION['ddes'];?>">C$ <?php echo number_format($row->venta,2,",","."); ?></a></td>
+                                    <td><a href="caja.php?idd=<?php echo $row->cod.'&ddes='.$_SESSION['ddes']; ?>"><?php echo $row->cant; ?></a></td>
+                                    <td class="green accent-2"><div align="left">CS <?php echo number_format($row->importe,2,",",".") ?></div></td>
+                                    <td>
+                                        <?php
+                                            if(($row->exitencia-$row->cant)>0){
+                                                echo $row->exitencia-$row->cant;
+                                            }else{
+                                                echo 0;
+                                            }
+                                        
+                                        ?>
+                                    </td>
+                                    <td><button class="waves-effect waves-light btn red" onClick="window.location='php_eliminar_caja.php?id=<?php echo $row->cod; ?>'"><i class="left material-icons">delete</i>Remover</button></td>
+                                </tr>
+                                <?php } ?>
+                          </tbody>
                       </table>
+                      
                   </div>
+                  
+                  <?php
+                        if(!empty($_GET['id'])){
+                      ?>
+                            <br><br><br>
+                            <form class="col s12" name="form2" method="get" action="php_caja_act.php">
+                                <div class="row">
+                                  <input class="validate" type="hidden" name="xcodigo" value="<?php echo $_GET['id']; ?>">
+                                    <div class="input-field col s2">
+                                        <input class="validate" type="text" name="xdes" id="xdes"  autocomplete="off">
+                                        <label for="">Nuevo Precio o % Descuento</label>
+                                    </div>
+                                    <div class="col s2">
+                                        <br><br>
+                                        <input type="radio" name="group1" id="test1" value="p"  checked>
+                                        <label for="test1">Descuento %</label>    
+                                    </div>
+                                    <div class="col s2">
+                                        <br><br>
+                                        <input type="radio" name="group1" id="test2" value="d">
+                                        <label for="test2">Nuevo Precio C$</label>
+                                    </div>
+                                    <div class="col s2">
+                                        <br><br>
+                                        <button type="submit" class="waves-effect waves-light btn green accent-4">Procesar</button>
+                                    </div>
+                                </div>
+                            </form>
+                      <?php } ?>
+                      <?php
+                        if(!empty($_GET['idd'])){
+                      ?>
+                            <form class="col s12" action="">
+                                <input type="hidden" name="xcodigo" id="xcodigo" value="<?php echo $_GET['idd']; ?>">
+                                <div class="row">
+                                    <div class="input-field col s2">
+                                        <input autofocus type="text" name="cantidad" id="cantidad" autocomplete="off" class="validate">
+                                        <label for="cantidad">Cantidad</label>
+                                    </div>
+                                    <div class="col s2">
+                                        <br><br>
+                                        <button type="submit" class="waves-effect waves-light btn green accent-4">Procesar</button>
+                                    </div>
+                                </div>
+                            
+                            </form>
+                      <?php } ?>
               </div>
               <br>
               <div class="row">
+                 
                   <div class="col s12 m4 l4">
+                      
                       <div class="chip center-align" style="width:100%;height:70px;">
                           <h4>0 Articulos en venta</h4>
                       </div>

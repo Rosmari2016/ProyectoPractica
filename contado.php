9d8dac8d16809ab0bce918b5edef0b7d6e0a2268
YuritzaSolis <yuritza.solis01@gmail.com>
2016-08-28 21:33:54 -0600

    casi

--- contado.php
+++ contado.php
@@ -54,9 +54,17 @@
 			main {
 				flex: 1 0 auto;
 			}
+             @media print{
+                #ocultar{
+                    display: none;
+                }
+            }
 	</style>
 	<link rel="shortcut icon" type="image/x-icon" href="ico/favicon.png">
     <script language="javascript">
+        function imprimir(){
+            this.print();
+        }
         function imprSelect(nombre){
             var ficha = document.getElementById(nombre);
             var ventimp = window.open(' ','popimpr');
@@ -69,11 +77,14 @@
 
 </head>
 <body>
-    <?php
-		if ($_SESSION['tipo_usu']=='a'){
-			require_once "menu_admin.php";
-		}
-	?>
+    <div id="ocultar">
+        <?php
+            if ($_SESSION['tipo_usu']=='a'){
+                require_once "menu_admin.php";
+            }
+        ?>
+    </div>
+    
 
     <main>
         <?php if($error='no'){ ?>
@@ -82,24 +93,98 @@
                 <a href="caja.php?ddes=0" class="waves-effect waves-light btn green accent-4"><i class="material-icons left">fast_rewind</i> Regresar Ventas</a>
                 <a class="waves-effect waves-light btn green accent-4" onclick="imprimir();"><i class="material-icons left">print</i> Imprimir Factura</a>
             </div>
+        <div/>
+            <br>
             <div id="impresion">
                 <div class="row">
-                    <div class="col s6">
+                    <div class="col s12 m12 l6">
                         <div class="row">
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
-                            <div class="chips col s12" style="font-size:24px"><strong></strong></div>
+                            <div class="chip col s12 m12 l12 center-align green-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>Total a Pagar</strong></div>
+                            <div class="chip col s12 m12 l12 center-align black-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>C$ <?php echo number_format($tpagar,2,",","."); ?></strong></div>
+                            <div class="chip col s12 m12 l12 center-align green-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>Dinero Recibido</strong></div>
+                            <div class="chip col s12 m12 l12 center-align black-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>C$ <?php echo number_format($ccpago,2,",","."); ?></strong></div>
+                            <div class="chip col s12 m12 l12 center-align green-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>Cambio</strong></div>
+                            <div class="chip col s12 m12 l12 center-align black-text" style="font-size:24px;padding-bottom:40px;padding-top:20px;"><strong>C$ <?php echo number_format($cambio,2,",","."); ?></strong></div>
+                            
+                            <div id="ocultar" class="alert-success col s12 m12 l12 center-align green-text" style="font-size:1.3em;margin-top:30px;margin-bottom:25px;">
+                                <strong>Pago Realizado con exito</strong><br><a href="caja.php?ddes=0">Regresar a la caja</a>
+                            </div>
+                            
+                            <center><a class="waves-effect waves-light btn-large green accent-4" onclick="imprimir();"><i class="material-icons left">print</i> Imprimir Factura</a></center>
+                       
+                            
                         </div>
                     </div>
-                    <div class="col s6">
-                    
+                    <div class="col s12 m12 l6">
+                        <!-- codigo imprimir -->
+                        <?php
+                            $can=$db->mysqli->query("SELECT * FROM empresa WHERE id=1");
+                            if($row=$can->fetch_object()){
+                                $empresa=$row->empresa; $direccion=$row->direccion;
+                                $telefono=$row->tel1;   $nit=$row->nit;
+                                $fecha=date("Y-m-d H:i:s"); $pagina=$row->web;
+                                $tama=$row->tamano;
+                            }
+                            $can=$db->mysqli->query("SELECT usuarios.nom AS cajera FROM factura INNER JOIN usuarios ON usuarios.usu=factura.cajera WHERE factura='$factura'");
+                            if($row=$can->fetch_object()){
+                                $cajera=$row->cajera;
+                            }
+                           
+                        ?>
+                        <div id="Imprime" style="font-size:<?php echo $tama.'px;'; ?>">
+                            <table width="100%">
+                                <tr>
+                                    <td>
+                                        <strong><?php echo $empresa; ?></strong><br/>
+                                        <?php echo $direccion;?><br/>
+                                        <?php echo $telefono;?><br/>
+                                        <?php echo $nit; ?><br/>
+                                    </td>
+                                    <td class="right-align"><?php echo $fecha;?></td>
+                                </tr>
+                                <tr>
+                                    <td>Cajero/a: <?php echo $cajera;?></td>
+                                </tr>
+                            </table>
+                            <br>
+                            <table width="100%" class="responsive-table">
+                                <thead>
+                                    <tr>
+                                        <th>CANT</th>
+                                        <th>DESCRIPCIÓN</th>
+                                        <th class="right-align">IMPORTE</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                <?php $numero=0;$valor=0;
+                                    $query=$db->mysqli->query("SELECT * FROM detalle WHERE factura='$factura'");
+                                    while($row=$query->fetch_object()){
+                                        $numero+=1;
+                                        $valor+=$row->valor;
+                                        $tipo=$row->tipo;
+                                ?>
+                                        <tr>
+                                            <td><?php echo $row->cantidad; ?></td>
+                                            <td><?php echo $row->nombre; ?></td>
+                                            <td class="right-align">C$ <?php echo number_format($row->valor,2,",","."); ?></td>
+                                        </tr>
+                                <?php } ?>
+                                </tbody>      
+                            </table>
+                            <div class="center-align">
+                                NO. DE ARTICULOS: <?php echo $numero; ?><br>
+                                <strong>* VENTA A <?php echo $tipo; ?>*</strong><br>
+                                <strong>TOTAL: C$ <?php echo number_format($valor,2,",",".");?></strong>
+                                <p>FIRMA DEL CLIENTE</p>
+                                <p>__________________________</p>
+                                 GRACIAS POR SU COMPRA<br>
+                                <?php echo $pagina; ?>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
-        </div>
         <?php } ?>
     </main>  
 
